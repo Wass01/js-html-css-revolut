e4525bbc0b11d6163be6bc9691dd63c7dd0ddecc
wass01 <maatallawassim01@gmail.com>
2021-03-12 16:24:25 +0100

    dropmenu prova

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-
+  dropDown();
 });
+
+function dropDown() {
+  var over = $(".over-drop > a");
+
+  over.mouseenter(function() {
+    $("over-drop > div").addClass("active");
+  });
+}
